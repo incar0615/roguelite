d93adf2a2e5ea831921331cbda989a2c31f3a214
전충헌 <incar0615@gmail.com>
2021-01-18 12:13:38 +0900

    [던전] 던전 생성 관련 데이터

---
+++ Assets/GameObjects/Dungeon/Room.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace P1
+{
+    namespace GameObjects
+    {
+        public class Room
+        {
+            public int gridX;
+            public int gridY;
+            public RoomType type;
+            public int depth;
+
+            // Right, Left, Top, Bottom 순서  
+            // 0 = closed, 1 = opened
+            public int doorMask = (int)Direction.ALL_CLOSE;
+
+            public Room(int xPos, int yPos, RoomType rType, int depth = 9999)
+            {
+                gridX = xPos;
+                gridY = yPos;
+                type = rType;
+                this.depth = depth;
+            }
+
+            public void ChangeDoorMask(int direction)
+            {
+                doorMask |= direction;
+            }
+
+            public bool IsOpenedDirection(int direction)
+            {
+                return (doorMask | direction) == doorMask ? true : false;
+            }
+
+        }
+    }
+}
