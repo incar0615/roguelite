828a6ed1e352725d9e017018414ec19639aef82a
전충헌 <incar0615@gmail.com>
2021-01-25 16:18:56 +0900

    EventManager 추가

---
+++ Assets/Scripts/Event/IEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace P1
+{
+    public interface IEventHandler
+    {
+
+        /// <summary>
+        /// Subscribe to events
+        ///
+        /// @example
+        ///   Events.AddListener<MoveResolvedEvent>(OnMoveResolved);
+        ///     or
+        ///   EventManager.OnSetRule += OnSetRule;
+        /// </summary>
+        void SubscribeEvents();
+
+        /// <summary>
+        /// Unsubscribe from events
+        ///
+        /// @example
+        ///   Events.RemoveListener<MoveResolvedEvent>(OnMoveResolved);
+        ///     or
+        ///   EventManager.OnSetRule -= OnSetRule;
+        /// </summary>
+        void UnsubscribeEvents();
+
+    }
+}
