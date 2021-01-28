13477cda7970e1b240f548d0355d09c22c8a4547
전충헌 <incar0615@gmail.com>
2021-01-28 14:28:49 +0900

    LoadingManager 및 LoadingScene 생성

---
+++ Assets/Scripts/Manager/LoadingManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace P1
+{
+    public class LoadingManager : MonoBehaviour
+    {
+        static string nextSceneName;
+        static LoadSceneMode loadSceneMode;
+
+        [SerializeField]
+        Image progressBar;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if(nextSceneName != null & nextSceneName != "")
+            {
+                StartCoroutine(LoadScene());
+            }
+        }
+
+        public static void LoadScene(string sceneName, LoadSceneMode mode = LoadSceneMode.Single)
+        {
+            nextSceneName = sceneName;
+            loadSceneMode = mode;
+            SceneManager.LoadScene("LoadingScene");
+        }
+
+        public static void UnloadLoadingScene()
+        {
+            SceneManager.UnloadSceneAsync("LoadingScene");
+        }
+
+        public void LoadSceneAsync(string sceneName, LoadSceneMode mode)
+        {
+            SceneManager.LoadSceneAsync(sceneName, mode);
+        }
+
+        IEnumerator LoadScene()
+        {
+            AsyncOperation op = SceneManager.LoadSceneAsync(nextSceneName, loadSceneMode);
+
+            op.allowSceneActivation = false;
+
+            float timer = 0.0f;
+
+            while (!op.isDone)
+            {
+                yield return null;
+
+                timer += Time.deltaTime;
+                if (op.progress < 0.9f)
+                {
+                    progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, op.progress, timer);
+                    if (progressBar.fillAmount >= op.progress)
+                    {
+                        timer = 0f;
+                    }
+                }
+                else
+                {
+                    progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, 1f, timer);
+                    if (progressBar.fillAmount == 1.0f)
+                    {
+                        op.allowSceneActivation = true;
+                        nextSceneName = "";
+                        loadSceneMode = LoadSceneMode.Single;
+                        yield break;
+                    }
+                }
+            }
+        }
+    }
+
+}
