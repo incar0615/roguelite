533e40f795d8b35e23ea236f14b1416bb79986f8
전충헌 <incar0615@gmail.com>
2021-01-29 10:17:14 +0900

    [UI] 게임씬 UI 컨트롤러 추가 및 HpChangedEvent 추가

---
+++ Assets/Scripts/Event/HpChangedEvent.cs
@@ -0,0 +1,18 @@
+﻿using P1.GameObjects;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace P1
+{
+    /// <summary>
+    /// Base event for all EventManager events.
+    /// </summary>
+    public class HpChangedEvent : Event
+    {
+        public BattleObject objectData;
+        public float maxHp;
+        public float curHp;
+    }
+
+}
---
+++ Assets/Scripts/GameScene/GameSceneUIController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using P1.GameObjects;
+
+namespace P1
+{
+    public class GameSceneUIController : EventHandler
+    {
+        public Text hpText;
+        public Image hpProgressImg;
+
+        public Text goldText;
+        public Text gemText;
+        public Text contractText;
+
+        public Image leftMagicImg;
+        public Image rightMagicImg;
+
+        public Image scrollMagicIcon;
+        public Image scrollMagicProgressImg;
+
+
+        Coroutine hpChangedCoroutine;
+        Coroutine skillUsedCoroutine;
+        public override void SubscribeEvents()
+        {
+            EventManager.Instance.AddListener<HpChangedEvent>(OnHpChanged);
+        }
+
+        public override void UnsubscribeEvents()
+        {
+            EventManager.Instance.RemoveListener<HpChangedEvent>(OnHpChanged);
+        }
+        #region HpChanged
+        // HpChanged
+        void OnHpChanged(HpChangedEvent e)
+        {
+            hpChangedCoroutine = StartCoroutine(HpChangedRoutine(e));   
+        }
+
+        IEnumerator HpChangedRoutine(HpChangedEvent e)
+        {
+            yield return null;
+
+            BattleObject objectData = e.objectData;
+
+            float maxHp = e.maxHp;
+            float curHp = e.curHp;
+
+            hpText.text = curHp + " / " + maxHp;
+
+            hpProgressImg.fillAmount = curHp / maxHp;
+        }
+        #endregion
+
+        #region SkillUsed
+        void OnSkillUsed()
+        {
+
+        }
+
+        #endregion
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+
+    }
+
+}
