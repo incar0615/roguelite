cf8520a6ba346db6318e9067a741d7b965ce081d
전충헌 <incar0615@gmail.com>
2021-01-29 10:49:01 +0900

    [MessageEvent] SkillUsedEvent 추가

---
+++ Assets/Scripts/Event/SkillUsedEvent.cs
@@ -0,0 +1,22 @@
+﻿using P1.GameObjects;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace P1
+{
+    /// <summary>
+    /// Base event for all EventManager events.
+    /// </summary>
+    public class SkillUsedEvent : MessageEvent
+    {
+        EquipPart skillPart;
+        float cooldown;
+
+        public SkillUsedEvent(EquipPart skillPart, float cooldown)
+        {
+            this.skillPart = skillPart;
+            this.cooldown = cooldown;
+        }
+    }
+}
