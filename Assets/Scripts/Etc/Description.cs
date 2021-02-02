037a87413cad4d660556f5222edb341caedffec8
전충헌 <incar0615@gmail.com>
2021-02-03 03:56:16 +0900

    [기타] 주의사항 노트용 Description.cs 생성

---
+++ Assets/Scripts/Etc/Description.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 주의사항 표기용
+/// </summary>
+public class Description : MonoBehaviour
+{
+    [Header("주의사항")]
+
+    [TextArea(3,10)]
+    public string NOTE;
+}
