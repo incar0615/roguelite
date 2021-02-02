896ee54366020c215cc02314ca57212b0785b5fe
전충헌 <incar0615@gmail.com>
2021-02-03 03:40:48 +0900

    [메세지이벤트] ItemGetEvent ItemEquipEvent 추가

---
+++ Assets/Scripts/Event/ItemEquipEvent.cs
@@ -0,0 +1,24 @@
+﻿using P1.GameObjects;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace P1.Events
+{
+    /// <summary>
+    /// Base event for all EventManager events.
+    /// </summary>
+    public class ItemEquipEvent : MessageEvent
+    {
+        public EquipPart part;
+        public ItemBase equippedItem;
+        public ItemBase unEquippedItem;
+
+        public ItemEquipEvent(EquipPart part, ItemBase equippedItem, ItemBase unEquippedItem)
+        {
+            this.part = part;
+            this.equippedItem = equippedItem;
+            this.unEquippedItem = unEquippedItem;
+        }
+    }
+}
---
+++ Assets/Scripts/Event/ItemGetEvent.cs
@@ -0,0 +1,20 @@
+﻿using P1.GameObjects;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace P1.Events
+{
+    /// <summary>
+    /// Base event for all EventManager events.
+    /// </summary>
+    public class ItemGetEvent : MessageEvent
+    {
+        public ItemBase item;
+
+        public ItemGetEvent(ItemBase item)
+        {
+            this.item = item;
+        }
+    }
+}
