981c9fc29b004e43c851da89eafc3c3aad00b52e
전충헌 <incar0615@gmail.com>
2021-02-04 03:54:16 +0900

    [매니저] 리소스를 로드 및 관리할 ResourceManager.cs 추가

---
+++ Assets/Scripts/Manager/ResourceManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace P1
+{
+    // TODO 나중에 에셋번들 사용하는쪽으로 변경해야 좋을듯 
+    public class ResourceManager : MonoBehaviour
+    {
+        #region singleton
+        private static ResourceManager instance;
+        public static ResourceManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    var obj = FindObjectOfType<ResourceManager>();
+                    if (obj != null)
+                    {
+                        instance = obj;
+                    }
+                    else
+                    {
+                        var newSingleton = new GameObject("ResourceManager").AddComponent<ResourceManager>();
+                        instance = newSingleton;
+                    }
+                }
+                return instance;
+            }
+            private set
+            {
+                instance = value;
+            }
+        }
+        #endregion
+    
+        public T LoadResourceByPath<T>(string path) where T : Object
+        {
+            return Resources.Load<T>(path);
+        }
+    }
+}
